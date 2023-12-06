6696d710de4bfd0decd4562ec3686611df162681
Korven13 <hedav05@gmail.com>
2023-12-06 11:11:07 +0100

    1

---
+++ src/Account.java
@@ -0,0 +1,12 @@
+public class Account {
+    String accName;
+    int accId;
+    int balance;
+
+    public Account() {
+        this.accName = accName;
+        this.accId = accId;
+        this.balance = balance;
+    }
+
+}
---
+++ src/User.java
@@ -0,0 +1,17 @@
+public class User {
+    String name;
+    String password;
+    int id;
+    String gender;
+    int age;
+    Account userAcc;
+
+    public User() {
+        this.name = name;
+        this.password = password;
+        this.id = id;
+        this.gender = gender;
+        this.age = age;
+    }
+
+}
